0a1fa0d9bf1e990bf5a2f09d56814db8570941e5
azulg <agarber@mail.austral.edu.ar>
2025-05-12 21:41:03 -0300

    app.py

--- registro-patente/backend/app.py
+++ registro-patente/backend/app.py
@@ -3,20 +3,23 @@ import subprocess
 
 app = Flask(__name__)
 
-
 @app.route('/capture', methods=['GET'])
 def capturar_y_procesar():
     try:
         print("📥 Solicitud recibida desde ESP32. Ejecutando controlador de captura...")
 
+        # Ruta correcta y completa al script en el servidor EC2
+        script_path = "/home/ubuntu/electronica-inf/registro-patente/backend/app/camera/rekognition_capture_controller.py"
+
         result = subprocess.run(
-            ["python3", "registro-patente/backend/app/camera/rekognition_capture_controller.py"],  # asegúrate de tener este script en el mismo directorio
+            ["python3", script_path],
             capture_output=True,
             text=True
         )
 
         print("🧠 Resultado del script:")
-        print(result.stdout)
+        print("STDOUT:", result.stdout)
+        print("STDERR:", result.stderr)
 
         if result.returncode == 0:
             return jsonify({"status": "ok", "output": result.stdout}), 200
@@ -26,7 +29,6 @@ def capturar_y_procesar():
     except Exception as e:
         print("⚠️ Error ejecutando script:", e)
         return jsonify({"status": "exception", "message": str(e)}), 500
-
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=False)
